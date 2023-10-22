081b55395e6f6b5d5113c560c98348689e991db9
kwagley0 <121137026+kwagley0@users.noreply.github.com>
2023-10-21 23:55:39 -0400

    First Commit

---
+++ src/config/firebaseConfig.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAZYhQf1L8w56ihKfb9cWVv5QUeVhWiV_M",
+    authDomain: "crypto-tracker-3cfba.firebaseapp.com",
+    projectId: "crypto-tracker-3cfba",
+    storageBucket: "crypto-tracker-3cfba.appspot.com",
+    messagingSenderId: "971401471715",
+    appId: "1:971401471715:web:6e858e130f5494eca787d6"
+  };
+
+export default firebaseConfig;
